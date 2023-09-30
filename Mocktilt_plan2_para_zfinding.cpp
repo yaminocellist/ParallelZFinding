aab9c9322d994940523830b7c7563e1b0b465cf4
yaminocellist <58488672+yaminocellist@users.noreply.github.com>
2023-09-30 23:44:32 +0800

    Update Mocktilt_plan2_para_zfinding.cpp

--- Mocktilt_plan2_para_zfinding.cpp
+++ Mocktilt_plan2_para_zfinding.cpp
@@ -8,7 +8,22 @@
 #include <math.h>
 #include <algorithm>
 #include <omp.h>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
 using namespace std;
+
+void remove_nan(std::vector<float>& vec) {
+    vec.erase(
+        std::remove_if(
+            vec.begin(),
+            vec.end(),
+            [](float value) { return std::isnan(value); }
+        ),
+        vec.end()
+    );
+}
 
 /*********************************************************************
  *                      GLOBAL VARIABLES;
@@ -42,9 +57,6 @@ float zScan (const vector<float> &gx, const vector<float> &gy, const vector<floa
     #pragma omp parallel for
     for (int s = 0; s < bins; s++) {
         int counter = 0;
-        float x1, y1, z1, rg1, rv1, theta1, eta1, phi1;
-        float x2, y2, z2, rg2, rv2, theta2, eta2, phi2;
-        int layer1, layer2;
         vector<float> eta_0, eta_1, eta_2;
         vector<float> phi_0, phi_1, phi_2;
         #pragma omp parallel for
@@ -134,23 +146,164 @@ float zScan (const vector<float> &gx, const vector<float> &gy, const vector<floa
 }
 
 /*********************************************************************
+ *              Another version of scanning:
+ * ******************************************************************/
+float zScan2 (const vector<float> &gx, const vector<float> &gy, const vector<float> &gz, const float &etaThreshold, const float &phiThreshold) {
+    vector<float> cnt(bins);
+    vector<float> phi_values, relative_r_values;
+    vector<float> eta_0, eta_1, eta_2;
+    vector<float> phi_0, phi_1, phi_2;
+    for (int j = 0; j < gx.size(); j++) {
+        float relative_x = gx[j];
+        float relative_y = gy[j];
+        float phi = atan2(relative_y, relative_x);
+        float relative_r = sqrt(relative_x * relative_x + relative_y * relative_y);
+        
+        if (relative_r >= 2.37 && relative_r <= 2.80) {
+		    eta_0.push_back(1.);            phi_0.push_back(1.);
+            eta_1.push_back(std::nan(""));  phi_1.push_back(std::nan(""));
+            eta_2.push_back(std::nan(""));  phi_2.push_back(std::nan(""));
+		}
+		else if (relative_r >= 3.14 && relative_r <= 3.59) {
+		    eta_0.push_back(std::nan(""));  phi_0.push_back(std::nan(""));
+            eta_1.push_back(1.);            phi_1.push_back(1.);
+            eta_2.push_back(std::nan(""));  phi_2.push_back(std::nan(""));
+		}
+		else if (relative_r >= 3.91 && relative_r <= 4.3925) {
+		    eta_0.push_back(std::nan(""));  phi_0.push_back(std::nan(""));
+            eta_1.push_back(std::nan(""));  phi_1.push_back(std::nan(""));
+            eta_2.push_back(1.);            phi_2.push_back(1.);
+		}
+        else {
+            eta_0.push_back(std::nan(""));  phi_0.push_back(std::nan(""));
+            eta_1.push_back(std::nan(""));  phi_1.push_back(std::nan(""));
+            eta_2.push_back(std::nan(""));  phi_2.push_back(std::nan(""));
+        }
+        phi_values.push_back(phi);
+        relative_r_values.push_back(relative_r);
+    }
+    
+    #pragma omp parallel for
+    for (int s = 0; s < bins; s++) {
+        int counter = 0;
+        std::vector<float> eta_0Dummy = eta_0;
+        std::vector<float> eta_1Dummy = eta_1;
+        std::vector<float> eta_2Dummy = eta_2;
+        std::vector<float> phi_0Dummy = phi_0;
+        std::vector<float> phi_1Dummy = phi_1;
+        std::vector<float> phi_2Dummy = phi_2;
+        #pragma omp parallel for
+        for (int i = 0; i < gx.size(); i++) {
+            // float x = gx[i];
+            // float y = gy[i];
+            float z = gz[i] - (zmin + s*scanstep);
+            // float phi = atan2(y, x);
+            // float rv = sqrt(x*x + y*y); float rg = sqrt(gx[i]*gx[i] + gy[i]*gy[i]);
+            float theta = atan2(relative_r_values[i], z);
+            float eta;
+            if (z >= 0) {
+                eta = -log(tan(theta/2));
+            }
+            else {
+                eta = log(tan((M_PI - theta)/2));
+            }
+            eta_0Dummy[i] *= eta;   eta_1Dummy[i] *= eta;   eta_2Dummy[i] *= eta;
+            phi_0Dummy[i] *= phi_values[i]; phi_1Dummy[i] *= phi_values[i]; phi_2Dummy[i] *= phi_values[i];
+        }
+
+        remove_nan(eta_0Dummy); remove_nan(eta_1Dummy); remove_nan(eta_2Dummy);
+        remove_nan(phi_0Dummy); remove_nan(phi_1Dummy); remove_nan(phi_2Dummy);
+
+        #pragma omp parallel for reduction(+:counter)
+        for (int i = 0; i < eta_0Dummy.size(); i++) {
+            for (int j = 0; j < eta_1Dummy.size(); j++) {
+                float d_eta = abs(eta_0Dummy[i] - eta_1Dummy[j]);
+                float d_phi = abs(phi_0Dummy[i] - phi_1Dummy[j]);
+                bool within_deta_range, within_dphi_range;
+                within_deta_range = d_eta < etaThreshold;
+                within_dphi_range = d_phi < phiThreshold;
+                if (within_deta_range && within_dphi_range ) {
+                    counter++;
+                }
+            }
+        }
+        #pragma omp parallel for reduction(+:counter)
+        for (int i = 0; i < eta_0Dummy.size(); i++) {
+            for (int j = 0; j < eta_2Dummy.size(); j++) {
+                float d_eta = abs(eta_0Dummy[i] - eta_2Dummy[j]);
+                float d_phi = abs(phi_0Dummy[i] - phi_2Dummy[j]);
+                bool within_deta_range, within_dphi_range;
+                within_deta_range = d_eta < etaThreshold;
+                within_dphi_range = d_phi < phiThreshold;
+                if (within_deta_range && within_dphi_range ) {
+                    counter++;
+                }
+            }
+        }
+        #pragma omp parallel for reduction(+:counter)
+        for (int i = 0; i < eta_1Dummy.size(); i++) {
+            for (int j = 0; j < eta_2Dummy.size(); j++) {
+                float d_eta = abs(eta_1Dummy[i] - eta_2Dummy[j]);
+                float d_phi = abs(phi_1Dummy[i] - phi_2Dummy[j]);
+                bool within_deta_range, within_dphi_range;
+                within_deta_range = d_eta < etaThreshold;
+                within_dphi_range = d_phi < phiThreshold;
+                if (within_deta_range && within_dphi_range ) {
+                    counter++;
+                }
+            }
+        }
+        eta_0Dummy.clear(); std::vector<float>().swap(eta_0Dummy);
+        eta_1Dummy.clear(); std::vector<float>().swap(eta_1Dummy);
+        eta_2Dummy.clear(); std::vector<float>().swap(eta_2Dummy);
+        phi_0Dummy.clear(); std::vector<float>().swap(phi_0Dummy);
+        phi_1Dummy.clear(); std::vector<float>().swap(phi_1Dummy);
+        phi_2Dummy.clear(); std::vector<float>().swap(phi_2Dummy);
+        cnt[s] = counter;
+    }
+    
+    auto most = max_element(cnt.begin(), cnt.end());
+    size_t max_i = most - cnt.begin();
+    float ctz = ((zmin + max_i*scanstep)*cnt[max_i] + (zmin + (max_i - 1)*scanstep)*cnt[max_i - 1] + (zmin + (max_i + 1)*scanstep)*cnt[max_i + 1]) 
+                / (cnt[max_i] + cnt[max_i - 1] + cnt[max_i + 1]);
+
+    cnt.clear(); phi_values.clear(); relative_r_values.clear();
+    std::vector<float>().swap(cnt);
+    std::vector<float>().swap(phi_values);
+    std::vector<float>().swap(relative_r_values);
+    eta_0.clear(); std::vector<float>().swap(eta_0);
+    eta_1.clear(); std::vector<float>().swap(eta_1);
+    eta_2.clear(); std::vector<float>().swap(eta_2);
+    phi_0.clear(); std::vector<float>().swap(phi_0);
+    phi_1.clear(); std::vector<float>().swap(phi_1);
+    phi_2.clear(); std::vector<float>().swap(phi_2);
+    return ctz;
+}
+/*********************************************************************
  *                      MAIN PROGRAMME;
  * ******************************************************************/
 int main (int argc, char *argv[]) {
     omp_set_num_threads(10);
-    ifstream myfile("plan2_tilted_upper_everyThing.txt");
-    // ifstream myfile("plan2_tilted_upper.txt");
+    ifstream myfile("plan2_tilted_upper_everyThing_0.9.txt");
 	if (!myfile .is_open()) {
 		cout << "Unable to open sorted file." << endl;
 		system("read -n 1 -s -p \"Press any key to continue...\" echo");
 		exit(1);
 	}
-    ofstream outFile("plan2_para_foundZ.txt", std::ios_base::app);
+    ofstream outFile("plan2_para_foundZ_0.9.txt", std::ios_base::app);
 	if (!outFile .is_open()) {
 		cout << "Unable to open modified file." << endl;
 		system("read -n 1 -s -p \"Press any key to continue...\" echo");
 		exit(1);
 	}
+
+    // Get current time as a time_point object
+    auto now = std::chrono::system_clock::now();
+    // Convert to a time_t object
+    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+    // Print the time
+    std::cout << std::put_time(std::localtime(&currentTime), "%F %T") << '\n';
+
 
     int event_num;
     int indicator = 990;    int cntr = 0;
@@ -174,11 +327,11 @@ int main (int argc, char *argv[]) {
             float re;
             if (cntr >= starter) {
                 if (gx.size() > ls_cut) {
-                    re = zScan(gx, gy, gz, threshold_eta_l, threshold_phi_l);
+                    re = zScan2(gx, gy, gz, threshold_eta_l, threshold_phi_l);
                 }else {
-                    re = zScan(gx, gy, gz, threshold_eta_s, threshold_phi_s);
+                    re = zScan2(gx, gy, gz, threshold_eta_s, threshold_phi_s);
                 }
-                outFile << indicator << "," << re << endl;
+                outFile << indicator << "," << re << "," << gx.size() << endl;
             }
             cntr++;
             indicator = event_num;
