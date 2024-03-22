e6084a7201c42075db84525d5a42ad7282d893f1
yaminocellist <realpklionmagnus@gmail.com>
2024-03-22 18:06:48 +0800

    routinely update

--- RootFileBuilder.C
+++ RootFileBuilder.C
@@ -11,6 +11,7 @@ void lite()
     // ifstream myfile("../Meeting5/foundZ_debug2_DCA_-8_8_-010_010_2e-1.txt");
     // ifstream myfile("./zFindingResults/foundZ_DCA_npeaks-8_8_-010_010_1e-1.txt");
     
+    
     if (!myfile.is_open()) {
 		  cout << "Unable to open text file" << endl;
 		  system("read -n 1 -s -p \"Press any key to continue...\" echo");
@@ -139,7 +140,8 @@ void full()
     // ifstream myfile("../Meeting5/foundZ_debug2_DCA_biased.txt");
     // ifstream myfile("../Meeting5/foundZ_debug2_DCA_-8_8_-010_010_2e-1.txt");
     // ifstream myfile("./zFindingResults/foundZ_DCA_npeaks-8_8_-010_010_1e-1.txt");
-    ifstream myfile("./zFindingResults/foundZ_allInfo_DCA_0_16_-001_001_2e-1.txt");
+    // ifstream myfile("./zFindingResults/foundZ_allInfo_DCA_0_16_-001_001_2e-1.txt");
+    ifstream myfile("./zFindingResults/foundZ_MBD0_10_DCA_0_16_-001_001_2e-1.txt");
     
     if (!myfile.is_open()) {
 		  cout << "Unable to open text file" << endl;
